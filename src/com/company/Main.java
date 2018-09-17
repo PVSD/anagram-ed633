f3b20023bf6e220866f3a5c28dce9e6b109fecb3
ed633 <edendecolli@icloud.com>
2018-09-17 11:21:13 -0400

    hjjkjk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReadingString = new Scanner(System.in);
+        Scanner kbReadingInt = new Scanner(System.in);
+
+
+        System.out.println("Type in a word.");
+
+        String sign = kbReadingString.next();
+        System.out.println("Type in another word.");
+        int word1 = kbReadingInt.nextInt();
+        int word2 = kbReadingInt.nextInt();
+
+        int n1 = word1.length;
+        int n2 = word2.length;
+
     }
 }
